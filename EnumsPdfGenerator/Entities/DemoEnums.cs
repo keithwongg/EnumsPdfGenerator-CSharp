52700eab51800abf136af80cf0133cf7a4874cef
Keith Wong <keithwjh@outlook.sg>
2024-09-06 17:38:41 +0800

    Spamm enums to test page break in pdf generation

--- EnumsPdfGenerator/Entities/DemoEnums.cs
+++ EnumsPdfGenerator/Entities/DemoEnums.cs
@@ -16,3 +16,185 @@ public enum DemoStatus
     Processed = 300,
 
 }
+
+public enum DemoStatus1
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+
+public enum DemoStatus2
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus3
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus4
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus5
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus6
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus7
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus8
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus9
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus10
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus11
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus12
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus13
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus14
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
+public enum DemoStatus15
+{
+    [Display(Name = "Initiated Job")]
+    Initiated = 100,
+
+    [Display(Name = "Processing File")]
+    Processing = 200,
+
+    [Display(Name = "Processed File")]
+    Processed = 300,
+
+}
